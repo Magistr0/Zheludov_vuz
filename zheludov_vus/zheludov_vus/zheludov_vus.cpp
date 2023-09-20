59776a21aaf250f163ac90a8b18309290f5efb1d
Mag_istro <zheludov.tima@mail.ru>
2023-09-21 00:49:55 +0300

    lab0

---
+++ zheludov_vus/zheludov_vus/zheludov_vus.cpp
@@ -0,0 +1,18 @@
+﻿//test 1
+#include <iostream>
+//test
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    std::cout << "Hello World!\n";
+    int num;
+    char liter;
+
+    std::cout << "Введите символ\n";
+    std::cin >> liter;
+
+    std::cout << "Введите цифру\n";
+    std::cin >> num;
+
+    std::cout << "Ваша цифра:" << num << "\n" << "Ваша буква:" << liter;
+}
